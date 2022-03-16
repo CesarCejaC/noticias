f1c872bf2d588a639d40e696c2a2ff62c9fc434e
CesarCejaC <tonycejacortes@gmail.com>
2022-03-16 08:22:19 -0600

    aa

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -5,16 +5,14 @@ import { NoticiaService } from 'src/app/servicio/noticia.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
-export class Tab1Page implements OnInit{
-
+export class Tab1Page implements OnInit {
   constructor(private noticias: NoticiaService) {}
 
   ngOnInit(): void {
-    this.noticias.lineas().subscribe(articulos=>{
+    this.noticias.lineas().subscribe((articulos) => {
       console.log(articulos);
-    })
+    });
   }
-
 }
